ee05008c5ae8a4f715999bf217f6ed254a791b2c
Samuel MARTIN MORO <faust64@gmail.com>
2021-06-13 12:55:03 +0200

    revert(2936eea819ef8c61e53afaf406b7a3709f508abf): part of 1.1.2

--- app/controllers/bigbluebutton_api_controller.rb
+++ app/controllers/bigbluebutton_api_controller.rb
@@ -281,7 +281,6 @@ class BigBlueButtonApiController < ApplicationController
     end
     query = query.with_recording_id_prefixes(params[:recordID].split(',')) if params[:recordID].present?
     query = query.where(meeting_id: params[:meetingID].split(',')) if params[:meetingID].present?
-    query = query.where(state: params[:state].split(',')) if params[:state].present?
 
     @recordings = query.order(starttime: :desc).all
     @url_prefix = "#{request.protocol}#{request.host}"
